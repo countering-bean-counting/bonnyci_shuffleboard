8e7cf982e24f02cd961d9b6c39c0088ebbf60798
Augustina Ragwitz <augustina.ragwitz@ibm.com>
2016-12-22 18:04:22 -0800

    Create an abstract writer class for output
    Create an abstract class to enforce a "write" method. This is to
    guarantee consistency as additional output formats are required.
    
    The first implementation of this abstract class is with a print to
    stdout.
    
    Part of Issue #34
    
    Signed-off-by: Augustina Ragwitz <augustina.ragwitz@ibm.com>

--- shuffleboard/shuffleboard.py
+++ shuffleboard/shuffleboard.py
@@ -1,1 +1,20 @@
 # -*- coding: utf-8 -*-
+import requests
+import pprint
+
+import github_api
+import plunder
+
+
+class EventsCLIWriter(plunder.CLIWriter):
+
+    def __init__(self):
+        super()
+        self.printer = pprint.pprint
+
+    def write(self, events):
+        for (repo, typed_events) in events.items():
+            self.printer(repo)
+            for (typed_event_name, event_list) in typed_events.items():
+                self.printer(typed_event_name)
+                self.printer(event_list, depth=3)
